fd4a12106e5090c1a8fcf5801eaf4cea303c302d
Chang <andrewchang@utexas.edu>
2023-08-13 18:41:35 -0500

    project card thumbnail

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,14 +1,16 @@
 import { Project } from "@/app/projects/page"
+import Image from "next/image"
 
-export default function Card({title, description, img, date}: Project): React.ReactNode {
+export default function Card({title, description, imgSrc, date}: Project): React.ReactNode {
   return (
     <div className="flex rounded-md p-4 shadow-md hover:shadow-xl">
-      <div className="flex">
+      <div className="flex gap-2">
         <div>
           <h1 className="text-lg font-bold">{title}</h1>
           <p className="text-sm">{description}</p>
           <p className="text-sm">{date}</p>
         </div>
+        <Image className="rounded-lg sm:h-44 sm:w-52" src={imgSrc} height={100} width={100} alt="project thumbnail"/>
       </div>
     </div>
   )
