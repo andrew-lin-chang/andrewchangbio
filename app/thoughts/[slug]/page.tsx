cc7eed5ed2f36d4cfcdb25a2cdc8dba9296dabef
Chang <andrewchang@utexas.edu>
2023-08-30 23:16:30 -0500

    add first blog

---
+++ app/thoughts/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+// statically generate routes for posts at build time
+export async function generateStaticParams() {
+  const files = fs.readdirSync(path.join("posts"));
+  const paths = files.map((filename) => ({
+    slug: filename.replace(".mdx", ""),
+  }));
+
+  return paths;
+}
+
+function getPost({ slug }: { slug: string }) {
+  const markdownFile = fs.readFileSync(
+    path.join("posts", slug + ".mdx"),
+    "utf-8"
+  );
+
+  const { data: frontMatter, content } = matter(markdownFile);
+
+  return {
+    frontMatter,
+    slug,
+    content,
+  };
+}
+
+export default function Post({ params }: any) {
+  const props = getPost(params);
+
+  return (
+    <article className="prose dark:prose-invert mx-0 max-w-none">
+      <h1 className="font-bold text-4xl">{props.frontMatter.title}</h1>
+      <MDXRemote source={props.content} />
+    </article>
+  );
+}
--- app/thoughts/page.tsx
+++ app/thoughts/page.tsx
@@ -1,10 +1,39 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Card from "@/components/Card";
+
 export default function Thoughts() {
+  // find all files in posts directory
+  const postsDir = "posts";
+  const files = fs.readdirSync(path.join(postsDir));
+
+  // for each post found
+  const posts = files.map((filename) => {
+    const fileContent = fs.readFileSync(path.join(postsDir, filename), "utf-8");
+
+    const { data: frontMatter } = matter(fileContent);
+
+    return {
+      meta: frontMatter,
+      slug: filename.replace(".mdx", ""),
+    };
+  });
+
+  const thoughts = posts.filter((post) => post.meta.type === "blog");
+  
   return (
     <>
       <h1 className="text-4xl font-bold">Thoughts</h1>
       <p>You&apos;ll find my blog posts here</p>
 
-      <p className="mt-10 text-center text-3xl">In development!</p>
+      <div className="grid grid-cols-1 gap-4 mt-6">
+        {thoughts.map((thought) => {
+          return (
+            <Card meta={thought.meta} slug={thought.slug} key={thought.slug} />
+          );
+        })}
+      </div>
     </>
   );
 }
