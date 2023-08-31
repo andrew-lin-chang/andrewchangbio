9edabb4857e4ff454aa7e1edce07a0cb09eae943
Chang <andrewchang@utexas.edu>
2023-08-30 23:16:07 -0500

    fix card image sizes

--- components/Card.tsx
+++ components/Card.tsx
@@ -17,29 +17,35 @@ export default function Card({
         <div>
           <div className="my-2">
             <Link
-              href={"/projects/" + slug}
+              href={`${meta.type === "project" ? "/projects/" : "/thoughts/" }` + slug}
               className="text-xl font-bold hover:text-indigo-500 hover:underline"
             >
               {meta.title}
             </Link>
             <p className="text-sm text-gray-500">{meta.date}</p>
           </div>
-          <p className="text-sm md:text-md max-w-2xl mr-4">{meta.description}</p>
+          <p
+            className={`text-sm md:text-md ${
+              meta.imgSrc ? "max-w-2xl mr-4" : ""
+            }`}
+          >
+            {meta.description}
+          </p>
           <div className="mt-4 flex gap-4">
             {meta.repo && <GitHubRepoButton repo={meta.repo} />}
             {meta.visit && <Visit link={meta.visit} />}
           </div>
         </div>
 
-        <div className="w-50 h-50 grid place-items-center">
-          <Image
-            className="rounded-lg"
-            src={meta.imgSrc}
-            width={200}
-            height={200}
-            alt="project thumbnail"
-          />
-        </div>
+        {meta.imgSrc && (
+          <div className="w-50 h-50 grid place-items-center">
+            <img
+              className="max-w-[8rem] h-40 w-40 rounded-lg object-contain md:object-cover md:max-w-[10rem]"
+              src={meta.imgSrc}
+              alt="project thumbnail"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
