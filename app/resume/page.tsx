319a04f91a7cb654a992c6dc00dd07f8da7f4443
achang <andrewchang@utexas.edu>
2024-10-09 19:39:03 -0500

    updated resume fall 2024

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -5,7 +5,13 @@ export default function Resume() {
     <div>
       <h1 className="font-bold text-4xl">Resume</h1>
       <p>Feel free to download my resume!</p>
-      <iframe className="w-full h-[30rem] sm:h-screen mt-4" src="https://drive.google.com/file/d/1mxflGKLO25Soc4EOBbqI65HXZF-4w7nt/preview" width="640" height="480" allow="autoplay"></iframe>
+      <iframe
+        className="w-full h-[30rem] sm:h-screen mt-4"
+        src="https://drive.google.com/file/d/12i3id63N-V9KPShrtj7u6XelcLfmDI61/preview"
+        width="640"
+        height="480"
+        allow="autoplay"
+      ></iframe>
     </div>
   );
 }
